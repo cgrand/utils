11a2053080cdd8c4eb021cfca143b4cf3e09cbec
Christophe Grand <christophe@cgrand.net>
2012-09-24 09:45:39 +0200

    add net.cgrand group id

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject utils "0.1.0-SNAPSHOT"
+(defproject net.cgrand/utils "0.1.0-SNAPSHOT"
   :description "Useful functions and extensible macros"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
